2df657cae13586d2b41a87c520d240d5789e237b
pafullgarg <gargprafull55@gmail.com>
2026-01-26 20:22:50 +0530

    Revision spread and rest operator

--- js-revision/play.js
+++ js-revision/play.js
@@ -1,18 +1,22 @@
-// arrow functions allows to use properties of the object it is defined in using this keyword.
-// const name = "garg"
-const person = {
-    name: 'prafull',
-    age: 29,
-    greet() { 
-        console.log("Hi " + this.name)
-    }
-}
-
 const hobbies = ['Music', 'Cooking', 'Coding']
 // for (let hobby of hobbies){
 //     console.log(hobby) 
 // }
 
-console.log(hobbies.map(hobby => 'Hobby ' + hobby));
-console.log(person)
-person.greet();
+// for reference types const only keeps the location and type const where variable is pointing it doesnt keep the value constent
+hobbies.push('Programming')
+console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+
+const copyhobbies1 = hobbies.slice();
+console.log(copyhobbies1);
+
+// if using ... for breaing array in individual elements then its called spread operator.
+const copyhobbies2 = [...hobbies]
+console.log(copyhobbies2)
+
+// if using ... for merging multiple arguments into single array then its called rest operator.
+const toArray = (...args) => {
+    return args;
+}
+
+console.log(toArray(1,2,3,45,56,2));
