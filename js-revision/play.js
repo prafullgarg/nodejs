305b9a07b6b536c30b16a76ff34a56d0bd29a9bf
pafullgarg <gargprafull55@gmail.com>
2026-01-26 20:07:57 +0530

    Revision array methods

--- js-revision/play.js
+++ js-revision/play.js
@@ -8,4 +8,11 @@ const person = {
     }
 }
 
-person.greet()
+const hobbies = ['Music', 'Cooking', 'Coding']
+// for (let hobby of hobbies){
+//     console.log(hobby) 
+// }
+
+console.log(hobbies.map(hobby => 'Hobby ' + hobby));
+console.log(person)
+person.greet();
