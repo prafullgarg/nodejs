9a02ef7139d1d03fdd4a255e0b28abef706b3df4
pafullgarg <gargprafull55@gmail.com>
2026-01-26 20:03:02 +0530

    Revision objects, properties, methods

--- js-revision/play.js
+++ js-revision/play.js
@@ -1,23 +1,11 @@
-const name = 'prafull';
-let age  =  29;
-const hobbies = true ;
-
-//  name function can be created withor without function keyword, fucntion without function keywords are arrow functions.
-const summerizeUser = (userName, userAge, userHobbies) => {
-    if (userName) {
-    return ('Name is ' + userName + ' age is ' +  userAge + ' user has hobbies ' + userHobbies) ;
-    }
-    else{
-        return (name + " " + age + " " + " " + hobbies)
+// arrow functions allows to use properties of the object it is defined in using this keyword.
+// const name = "garg"
+const person = {
+    name: 'prafull',
+    age: 29,
+    greet() { 
+        console.log("Hi " + this.name)
     }
 }
 
-// const add = (a,b) => {
-//     return a + b ;
-// } 
-// if function is has onle single line return statment then it can be written as
-
-const add = (a,b) => a + b;
-
-console.log(add(1,2))
-console.log(summerizeUser(name, age, hobbies))
+person.greet()
