082939d676ea6361421ddc5ec96eeca22146cfff
pafullgarg <gargprafull55@gmail.com>
2026-01-26 20:49:04 +0530

    Revison async and promises

--- js-revision/play.js
+++ js-revision/play.js
@@ -1,17 +1,23 @@
-// Array and Object destructuring 
-const person = {
-    name: 'prafull',
-    age: 29
-} 
+const fetchData = () => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Done!');
+    }, 1500);
+  });
+  return promise;
+};
 
-const hobbies = ['Music', 'Cooking', 'Coding']
+setTimeout(() => {
+  console.log('Timer is done!');
+  fetchData()
+    .then(text => {
+      console.log(text);
+      return fetchData();
+    })
+    .then(text2 => {
+      console.log(text2);
+    });
+}, 2000);
 
-const printName = ({ name }) => console.log(name);
-printName(person)
-
-const { name, age} = person
-console.log ( name, age );
-
-const [hobby1, hobby2] = hobbies
-console.log(hobby1, hobby2)
-
+console.log('Hello!');
+console.log('Hi!');
