fe2b1d54e91f3a8a4ff9c1b5161bc27a6530d2de
pafullgarg <gargprafull55@gmail.com>
2026-01-29 08:17:31 +0530

    Printing request object as json

--- nodejs-basic/server.js
+++ nodejs-basic/server.js
@@ -4,6 +4,7 @@
 //  path without / means nodejs will search for module globally
 const http = require('http');
 const fs = require('fs')
+const path = require('path')
 
 const server = http.createServer((req, res) => {
     let body =[];
@@ -22,8 +23,16 @@ const server = http.createServer((req, res) => {
             socket: {
                 remoteAddres: req.socket.remoteAddress,
                 remotePort: req.socket.remotePort,
+                localAddress: req.socket.localAddress,
+                localPort: req.socket.localPort
             } 
         }
+        const filePath = path.join('nodejs-basic', 'full-request.json') 
+        fs.writeFileSync( filePath , JSON.stringify(fullRequest,null,2));
+        // when hitting localhost:3000 from browser it is adding some additional headers in req starting with sec-* 
+
+        res.writeHead(200,{'content-type': 'text/plain'})
+        res.end('fullRequest saved/n')
     } )
 });
 
