1e6003442260243524668ef2a52155d91bf2bc2b
pafullgarg <gargprafull55@gmail.com>
2026-01-29 21:53:53 +0530

    Sending custom response upon hitting url

--- nodejs-basic/server.js
+++ nodejs-basic/server.js
@@ -3,38 +3,20 @@
 
 //  path without / means nodejs will search for module globally
 const http = require('http');
-const fs = require('fs')
-const path = require('path')
 
 const server = http.createServer((req, res) => {
-    let body =[];
 
-    req.on('data', chunk => {body.push( chunk )} )
+        res.setHeader('content-type','text/html')
+        res.write('<html>');
+        res.write('<head><title>My First Page</title></head>');
+        res.write('<body><h1>Hello from my nodejs server!</h1></body>')
+        res.write('</html>')
+        res.end();
+        // as long as there is event handler without process.exit nodejs event loop will keep runing listening for requests.
+        process.exit();
 
-    req.on('end', ()=>{
-        body = Buffer.concat(body).toString();
-
-        const fullRequest = {
-            method: req.method,
-            url: req.url,
-            httpVersion: req.httpVersion,
-            headers: req.headers,
-            body: req.body,
-            socket: {
-                remoteAddres: req.socket.remoteAddress,
-                remotePort: req.socket.remotePort,
-                localAddress: req.socket.localAddress,
-                localPort: req.socket.localPort
-            } 
-        }
-        const filePath = path.join('nodejs-basic', 'full-request.json') 
-        fs.writeFileSync( filePath , JSON.stringify(fullRequest,null,2));
-        // when hitting localhost:3000 from browser it is adding some additional headers in req starting with sec-* 
-
-        res.writeHead(200,{'content-type': 'text/plain'})
-        res.end('fullRequest saved/n')
-    } )
-});
+    
+} );
 
 
 server.listen(3000)
