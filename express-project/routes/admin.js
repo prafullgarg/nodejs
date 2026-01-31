0f174c463dfc002c00efcff27ce3accf5240ad11
pafullgarg <gargprafull55@gmail.com>
2026-01-31 23:30:41 +0530

    Working on small shop application added routes , views , __dirname

--- express-project/routes/admin.js
+++ express-project/routes/admin.js
@@ -7,7 +7,6 @@ router.get("/add-product",(req,res,next) => {
     res.status(200).sendFile(path.join(__dirname,"..","views","add-product.html"));
 });
 
-
 // route is /admin/product
 router.post("/product",(req,res,next) => {
     console.log(req.body);
